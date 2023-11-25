917fd5f5c35593c1c3d82e4f1cc0b38f4a2bb1e5
Carlos Chamorro <cmchamorro99@hotmail.com>
2023-11-25 12:00:48 -0600

    Codigo hasta la clase 40/70, la siguiente es la 41

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,71 @@
+import { Component, Input, SimpleChange, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { repeat } from 'rxjs';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
+  counter = signal(0);
+  counterRef: number | undefined;
+
+  constructor(){
+    // NO ASYNC OPERATIONS
+    // before rendering
+    console.log('constructor');
+    console.log('-', repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChange){
+    //before and during rendering
+    console.log('ngOnChanges');
+    console.log('-', repeat(10));
+    console.log(changes);
+  }
+
+  ngOnInit(){
+    //after rendering
+    //una vez
+    //asunc, then, subs
+    console.log('ngOnInit');
+    console.log('-', repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+    if (typeof window !== "undefined") {
+      this.counterRef = Number(window.setInterval(() => {
+        console.log('run interval')
+        this.counter.update(statePrev => statePrev + 1);
+      },1000));
+    }
+  
+  }
+  
+
+  ngAfterViewInit(){
+    //after rendering
+    // hijos ya fueron pintados
+    console.log('ngAfterViewInit es despues del ngOnInit');
+    console.log('-', repeat(10));
+  }
+
+  ngOnDestroy(){  
+    //before destroying
+    console.log('ngOnDestroy');
+    console.log('-', repeat(10));
+    if (this.counterRef) {
+      clearInterval(this.counterRef);
+    }
+  }
+
+  doSomething(){
+    console.log('change duration');
+    console.log('-', repeat(10));
+  }
+}
